3ca1813f403b1b309ab25bf1c9541245e69c2bcc
Junkkah <hamalainenjani@hotmail.com>
2023-04-03 23:46:30 +0300

    in game info

--- map.py
+++ map.py
@@ -17,6 +17,18 @@ class Map(States):
         adventures = self.stats.map
         self.map_objects = [Adventure((adv["pos_x"], adv["pos_y"]), self.map_sprites, adv["desc"], adv["name"]) for adv in adventures.values()]
 
+        bubble = pg.image.load('./ab_images/map_bubble.png').convert_alpha()
+        hood = pg.image.load('./ab_images/hood.png').convert_alpha()
+        COORDS_BUBBLE  = (self.width * 0.12, self.height * 0.85)
+        COORDS_HOOD = (self.width * 0.05, self.height * 0.80)
+        SCALAR_BUBBLE = ((bubble.get_width() / 7), (bubble.get_height() / 7))
+        SCALAR_HOOD = ((hood.get_width() / 8), (hood.get_height() / 8))
+
+        self.bubble = pg.transform.scale(bubble, SCALAR_BUBBLE)
+        self.hood = pg.transform.scale(hood, SCALAR_HOOD)
+        self.bubble_rect = self.bubble.get_rect(bottomleft=COORDS_BUBBLE)
+        self.hood_rect = self.hood.get_rect(topleft=COORDS_HOOD)
+
     def get_event(self, event):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
@@ -37,6 +49,9 @@ class Map(States):
     def draw(self, screen):
         self.screen.blit(self.ground, (0,0))
         self.map_sprites.draw(self.screen)
+
+        self.screen.blit(self.bubble, self.bubble_rect)
+        self.screen.blit(self.hood, self.hood_rect)
 
         collided_objects = [obj for obj in self.map_objects if obj.rect.collidepoint(pg.mouse.get_pos())]
         if collided_objects:
