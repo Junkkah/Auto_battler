df1877939c2b4888d3005ba14c3ec9f1d1652cca
Junkkah <hamalainenjani@hotmail.com>
2024-07-13 22:03:40 +0300

    Added sounds docstrings

--- sounds_ab.py
+++ sounds_ab.py
@@ -13,26 +13,32 @@ SFX_volume = 0.3
 music_volume = 0.5
 
 def play_sound_effect(effect: str):
+    """Play a sound effect with the given filename."""
     sound_effect = pygame.mixer.Sound('./ab_sounds/' + effect + '.wav')
     sound_effect.set_volume(SFX_volume)
     sound_effect.play()
 
 def play_music_effect(location: str):
+    """Play background music with the given filename, looping indefinitely."""
     music_effect = pygame.mixer.Sound('./ab_sounds/' + location + '.wav')
     music_effect.set_volume(music_volume)
     if not pg.mixer.get_busy():
         music_effect.play(loops=-1)
 
 def set_sfx_volume(volume: float):
+    """Set the volume for sound effects."""
     global SFX_volume
     SFX_volume = volume
 
 def set_music_volume(volume: float):
+    """Set the volume for background music."""
     global music_volume
     music_volume = volume
 
 def get_sfx_volume() -> float:
+    """Return the current volume of sound effects."""
     return SFX_volume
 
 def get_music_volume() -> float:
+    """Return the current volume of background music."""
     return music_volume
